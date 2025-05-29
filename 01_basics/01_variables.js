5cc2af9d93abaafda5d95f15a3538172dcad5f50
SoumyadeepPal128 <sanupal328@gmail.com>
2025-05-29 13:55:00 +0000

    let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail="soumyadeep@google.com"
+var accountPassword="12345"
+accountCity ="Jaipur"
+
+//accountId=2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
